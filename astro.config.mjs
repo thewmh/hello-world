6ae51d6cf3b43b511f388368178fccc3e86ce6ee
William Harrison <wharrison@horizontal.com>
2022-12-15 18:31:41 -0500

    adds deploy info

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	site: 'https://thewmh.github.io',
+  	base: '/hello-world',
 });
